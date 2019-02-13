35b68a21399189071a87b24fddda56c917d4c866
rjagadee <rjagadee@LIN17000742.corp.capgemini.com>
2019-02-13 17:01:02 +0530

    Change-Id: I61418a83821837d27f5e6b1f0d5852f5fbe24b63 Signed-off-by: rjagadee <rjagadee@LIN17000742.corp.capgemini.com>

---
+++ src/com/java/core/generic/GenericMainTest.java
@@ -0,0 +1,14 @@
+package com.java.core.generic;
+
+public class GenericMainTest {
+
+	public GenericMainTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
