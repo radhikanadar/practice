2983e63b2d29ebe23e0abc2ebc005844a800522c
radnadar <Radhika.Nadar@partner.bmw.de>
2023-11-30 16:02:00 +0530

    Added practice files

--- hello/src/main/java/org/example/Final.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Final extends InterfaceANewImpl {
-
-    public static void main(String[] args) {
-        System.out.print(String.valueOf(null));
-    }
-}
--- hello/src/main/java/org/example/InterfaceAImpl.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class InterfaceAImpl implements InterfaceA {
-    @Override
-    public String getDefault() {
-        return "com.demo.java.InterfaceAImpl";
-    }
-}
